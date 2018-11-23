067d52d3fa2fdcd7cacc492efb7bea5e0ff82112
Muhammad <hussainmuhammad603@gmail.com>
2018-11-23 18:57:37 +0500

    first version of main and hungry code

---
+++ src/com/company/Hungray.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class   Hungray extends Main
+{
+    public void message()
+    {
+        System.out.println("Hungry file");
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+        Hungray hungray=new Hungray();
+        hungray.message();
+        System.out.println("Hello");
+
+    }
+}
